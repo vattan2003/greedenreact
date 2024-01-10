ec3c0105163a07d306d333f0a0d81551f867a490
vattan2003 <VATTANsince2003@gmail.com>
2024-01-10 15:00:58 +0530

    update responsive

--- src/nav.js
+++ src/nav.js
@@ -6,13 +6,14 @@ function Nav() {
     <nav>
       <h1 className="head">Greeden</h1>
 
-      {/* <div><i class="fa-solid fa-bars" onclick="slide_nav()"></i></div> */}
+       
 
       <ul className="nav_option">
         <button className="nav_option_btn"><a href="">home</a></button>
         <button className="nav_option_btn"><a href="">product</a></button>
         <button className="nav_option_btn"><a href="">contact</a></button>
       </ul>
+      <div className="menubar"><i class="fa-solid fa-bars"></i></div>
     </nav>
   )
 }
